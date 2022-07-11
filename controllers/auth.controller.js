406682ab341da8c885ba213402b126aa164b657a
Terrence Meikle <terrence_meikle@yahoo.com>
2022-07-11 16:47:18 -0400

    built folder structure

---
+++ controllers/auth.controller.js
@@ -0,0 +1,12 @@
+async function getSignup(req, res, next) {
+  //...
+}
+
+async function getLogin(req, res, next) {
+  //...
+}
+
+module.exports = {
+  getSignup: getSignup,
+  getLogin: getLogin,
+};
