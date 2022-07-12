8626222de142d226e38fd9c98d23f531e0931825
Terrence Meikle <terrence_meikle@yahoo.com>
2022-07-12 16:45:33 -0400

    added signup functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,26 @@
+const User = require('../models/user.model');
+
 async function getSignup(req, res, next) {
   res.render('customer/auth/signup');
 }
 
-async function signup(req, res, next) {}
+async function signup(req, res, next) {
+  const user = new User(
+    req.body.email,
+    req.body.password,
+    req.body.fullname,
+    req.body.street,
+    req.body.postal,
+    req.body.city
+  );
+
+  await user.signup();
+
+  res.redirect('/login');
+}
 
 async function getLogin(req, res, next) {
-  //...
+  res.render('customer/auth/login');
 }
 
 module.exports = {
