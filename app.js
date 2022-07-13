e5806349abcd46b875bcbfcac3d8568f95bcd239
Terrence Meikle <terrence_meikle@yahoo.com>
2022-07-13 04:32:08 -0400

    rearranged some file, added csurf, and server error handling

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 const express = require('express');
+const csrf = require('csurf');
 
 const db = require('./data/database');
+const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
 const authRoutes = require('./routes/auth.routes');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
 
 const app = express();
 
@@ -13,7 +16,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
+app.use(csrf());
+
+app.use(addCsrfTokenMiddleware);
+
 app.use(authRoutes);
+
+app.use(errorHandlerMiddleware);
 
 db.connectToDatabase()
   .then(function () {
