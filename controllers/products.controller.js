aa49cf82ba3142203fe113d19727101cd40ad1b1
Terrence Meikle <terrence_meikle@yahoo.com>
2022-07-17 17:49:36 -0400

    output products for customers

---
+++ controllers/products.controller.js
@@ -0,0 +1,16 @@
+const Product = require('../models/product.model');
+
+async function getAllProducts(req, res, next) {
+  let products;
+  try {
+    products = await Product.findAll();
+
+    res.render('customer/products/all-products', { products: products });
+  } catch (error) {
+    return next(error);
+  }
+}
+
+module.exports = {
+  getAllProducts: getAllProducts,
+};
